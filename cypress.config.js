2032cfb6f3f5c8c5102bdc213aed31f342e4fba2
Nataliia Bulatovskaia <dnatali2211@gmail.com>
2023-11-04 16:25:48 +0400

    Added Dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "amte81",
   e2e: {
     baseUrl: "http://qamid.tmweb.ru/admin/",
     setupNodeEvents(on, config) {
